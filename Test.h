c32b1273b4a8537c6b15be7562cdc261a14587a5
tschlitt <schlitt.thomas2016@myspartans.org>
2015-03-26 08:46:20 -0400

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by schlitt thomas on 3/26/15.
+//  Copyright (c) 2015 schlitt thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by schlitt thomas on 3/26/15.
+//  Copyright (c) 2015 schlitt thomas. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
